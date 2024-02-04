954542923160de7af4d3facc8808c5d80d8fb4f7
EderLimaDev <ederlimarh@gmail.com>
2024-02-04 13:35:13 -0300

    incluindo fetch

---
+++ src/components/ItemList/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.css';
+
+export const ItemList = (props) => {
+  return (
+    <div className='item-list'>
+        <strong>{props.title}</strong>
+        <p>{props.description}</p>
+        <hr />
+    </div>
+  )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,74 @@
-import { Header } from '../components/Header';
-import background from '../assets/background.png';
-import './styles.css';
-
+import { Header } from "../components/Header";
+import { useState } from "react";
+import background from "../assets/background.png";
+import "./styles.css";
+import { ItemList } from "../components/ItemList";
 
 export const Home = () => {
+  const [user, setUser] = useState("");
+  const [currentUser, setCurrentUser] = useState(null);
+  const [repos, setRepos] = useState(null);
+
+
+  const handleGetData = async () => {
+    const userData = await fetch(`https://api.github.com/users/${user}`)
+    const newUser = await userData.json();
+    
+    if(newUser.name) {
+      const {avatar_url, name, bio } = newUser
+      setCurrentUser({avatar_url, name, bio })
+
+      const reposData = await fetch(`https://api.github.com/users/${user}/repos`)
+      const newRepos = await reposData.json();
+
+      if(newRepos.length) {
+        setRepos(newRepos)
+      }
+
+
+    }
+  }
+
   return (
     <div>
-        <Header />
-        <div className='conteudo'>
-        <img src={background} alt="Bg github" className='background' />
-        <div className='info'>
+      <Header />
+      <div className="conteudo">
+        <img src={background} alt="Bg github" className="background" />
+        <div className="info">
           <div>
-          <input name='usuario' placeholder='@username' type="text" />
-          <button>Buscar</button>
-          
+            <input
+              name="usuario"
+              value={user}
+              onChange={(event) => setUser(event.target.value)}
+              placeholder="@username"
+              type="text"
+            />
+            <button onClick={handleGetData}>Buscar</button>
+          </div>
+          {/* {currentUser.name ? (): null} */}
+          <div className="perfil">
+            <img
+              src="https://avatars.githubusercontent.com/u/81269326?s=400&u=6ee8ae88cc938671a29b3290f7a39ae75baa6eac&v=4"
+              alt="imagem do Perfil"
+              className="profile"
+            />
+
+            <div>
+              <h3>Eder Lima</h3>
+              <span>@EderLimaDev</span>
+              <p>Descrição</p>
+            </div>
+          </div>
+          <hr />
+
+          <div>
+            <h4 className="repositorio">Repositórios</h4>
+            <ItemList title="teste" description="teste de descrição" />
+            <ItemList title="teste" description="teste de descrição" />
+            <ItemList title="teste" description="teste de descrição" />
           </div>
         </div>
-
-
-        </div>
-    
+      </div>
     </div>
-  )
-}
+  );
+};
